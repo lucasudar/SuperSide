10c743c64e56e94909aa1f1739cc209da0dfa826
Nikita Starkov <lucasudar@gmail.com>
2024-10-14 08:17:30 -0700

    terraform init with airflow

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "configure_kubectl" {
+  description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  value       = "aws eks --region ${local.region} update-kubeconfig --name ${module.eks.cluster_name}"
+}
+
+output "mwaa_webserver_url" {
+  description = "MWAA Webserver Url"
+  value       = module.mwaa.mwaa_webserver_url
+}
